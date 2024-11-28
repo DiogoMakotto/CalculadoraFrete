b9062a1887df9c742a722c73c5b9d94c18a439ca
DiogoMakotto <diogomakotto@gmail.com>
2024-11-27 21:26:26 -0300

    initial commit

---
+++ src/test/java/com/makotto/calculaFrete/CalculaFreteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makotto.calculaFrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculaFreteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
